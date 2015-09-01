1935df406c02b388e7050c7619144bfe4309bf46
ttu <tomi.tuhkanen@iki.fi>
2015-09-02 01:17:31 +0300

    Removed connection strings file and added it to gitignore

--- src/CubeSensorsRestApi/CubeSensorsRestApi/ConnectionStrings.fs
+++
@@ -1,4 +0,0 @@
-﻿module ConnectionStrings
-
-let LocalDb = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\SRC\GITHUB\CUBESENSORS-IOT-AZURE\SAMPLE_DATA\CUBE_DB.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-let Azure = @""
