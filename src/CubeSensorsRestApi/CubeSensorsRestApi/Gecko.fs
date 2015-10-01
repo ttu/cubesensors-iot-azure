7fca12f0d95af9c009a815e63466bdf312b427ec
ttu <tomi.tuhkanen@iki.fi>
2015-10-01 18:44:13 +0300

    Added Geckoboard parser

---
+++ src/CubeSensorsRestApi/CubeSensorsRestApi/Gecko.fs
@@ -0,0 +1,54 @@
+﻿module Gecko
+
+open System
+
+// List Type
+//[
+//  {
+//    "title": {
+//      "text": "Chrome"
+//    },
+//    "label": {
+//      "name": "New!",
+//      "color": "#ff2015"
+//    },
+//    "description": "40327 visits"
+//  },
+
+// Number Type
+//{
+//  "item": [
+//    {
+//      "value": 5723,
+//      "text": "Total paying customers"
+//    }
+//  ]
+//}
+
+type NumberItem = {
+    Value : string;
+    Text : string;
+}
+
+type NumberItemContent = {
+    Item : NumberItem[]
+}
+
+type ListTitle = {
+    Text : string;
+}
+
+type ListLabel = {
+    Name : string;
+    Color : string;
+}
+
+type ListItem = {
+    Title : ListTitle;
+    Label : ListLabel;
+    Description : string;
+}
+
+let WrapToNumber(content) =
+    let item = { NumberItem.Value = content.ToString(); NumberItem.Text = DateTime.Now.ToShortTimeString() }
+    { NumberItemContent.Item = [|item|] }
--- src/CubeSensorsRestApi/CubeSensorsRestApi/Program.fs
+++ src/CubeSensorsRestApi/CubeSensorsRestApi/Program.fs
@@ -31,15 +31,19 @@ let app =
           basicAuth // from here on it will require authentication
           path "/api/v1/sensor" >>= JSON (Db.GetSensorIds())
           path "/api/v1/sensor/status" >>= JSON (Db.GetSensorStatus(Db.getCurrentTime()))
-          pathScan "/api/v1/sensor/%s" (fun (id) -> JSON (Db.AllSensorData(id)))
           pathScan "/api/v1/sensor/%s/%d" (fun (id, min) -> JSON (Db.AllDataFromDuration(id, min)))
+          pathScan "/api/v1/sensor/%s" (fun (id) -> JSON (Db.AllSensorData(id)))
           pathScan "/api/v1/temperature/avg/%s/%d" (fun (id, min) -> JSON (Db.AvgTemperature(id, min)))
           pathScan "/api/v1/temperature/%s/%d" (fun (id, min) -> JSON (Db.TemperatureValuesFromDuration(id, min)))
           pathScan "/api/v1/noise/avg/%s/%d" (fun (id, min) -> JSON (Db.AvgNoise(id, min)))
+          pathScan "/api/v1/noise/avg/%s" (fun (id) -> JSON (Db.AvgNoiseDaily(id)))
           pathScan "/api/v1/noise/%s/%d" (fun (id, min) -> JSON (Db.NoiseValuesFromDuration(id, min)))
           pathScan "/api/v1/voc/avg/%s/%d" (fun (id, min) -> JSON (Db.AvgVoc(id, min)))
           pathScan "/api/v1/voc/%s/%d" (fun (id, min) -> JSON (Db.VocValuesFromDuration(id, min)))
           path "/api/v1/last" >>= JSON (Db.LastUpdate())
+          // These are Geckboard specifig
+          pathScan "/api/v1.1/noise/avg/%s" (fun (id) -> JSON (Gecko.WrapToNumber (Db.AvgNoiseDaily(id))))
+
         ]
       POST >>= choose
         [ path "/api/v1/hello" >>= OK "Hello POST"
@@ -47,14 +51,18 @@ let app =
        ] 
     ]
 
-[<EntryPoint>]
-let main argv =
-    match argv.Length with
-     | 0 -> startWebServer defaultConfig app
+let getConfig(argv :string[]) = 
+     match argv.Length with
+     | 0 -> defaultConfig
      | _ -> let port = Sockets.Port.Parse <| argv.[0]
             let serverConfig = 
                 { defaultConfig with
                    bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
                 }
-            startWebServer serverConfig app
+            serverConfig
+
+[<EntryPoint>]
+let main argv =
+    let config = getConfig(argv)
+    startWebServer config app
     0    
